520861d76f082da46a6f1091873120553abb12d1
Nathan Harris <nathanharris91@gmail.com>
2023-03-26 18:28:15 -0600

    second commit

--- assessment-backend/client/main.js
+++ assessment-backend/client/main.js
@@ -1,4 +1,10 @@
-const complimentBtn = document.getElementById("fortuneButton")
+const fortunetBtn = document.getElementById("fortuneButton")
+const postBtn = document.getElementById('addButton')
+const newFort = document.querySelector('#newFortune')
+const fortDiv = document.querySelector('#fortuneDisplay')
+const deleteBtn = document.querySelector('#deleteButton')
+const deleteInput = document.querySelector('#fortuneId')
+const allBtn = document.querySelector('#allButton')
 
 const getFortune = () => {
     axios.get("http://localhost:4000/api/fortune/")
@@ -8,4 +14,65 @@ const getFortune = () => {
     });
 };
 
-complimentBtn.addEventListener('click', getFortune)
+const postFortune = () => {
+    
+    fortDiv.innerHTML = ''
+
+    let bodyObj = {
+        name: newFort.value
+    }
+    
+    axios.post("http://localhost:4000/api/fortune/",bodyObj)
+    .then((res)=>{
+        console.log(res.data)
+
+        for(let i = 0; i < res.data.length; i++){
+            let newSpan = document.createElement('p')
+            newSpan.textContent = `${i + 1}. ${res.data[i]}`
+            fortDiv.appendChild(newSpan)
+        }   
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+    }
+
+    const deleteFortune = () => {
+        fortDiv.innerHTML = ''
+    
+        let idToDelete = deleteInput.value
+    
+        axios.delete(`http://localhost:4000/api/fortune/${idToDelete}`)
+            .then((res) => {
+                console.log(res.data)
+    
+                for(let i = 0; i < res.data.length; i++){
+                    let newSpan = document.createElement('p')
+                    newSpan.textContent = `${i + 1}. ${res.data[i]}`
+                    fortDiv.appendChild(newSpan)
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    const getAll = () => {
+        fortDiv.innerHTML = ''
+
+        axios.get("http://localhost:4000/api/fortune")
+        .then((res)=>{
+            console.log(res.data)
+
+            for (let i = 0; i < res.data.length; i++) {
+                let newSpan = document.createElement('p');
+                newSpan.textContent = `${i+1}.${res.data[i]}`
+                fortDiv.appendChild(newSpan)
+            }
+        })
+    }
+
+fortunetBtn.addEventListener('click', getFortune)
+postBtn.addEventListener('click',postFortune)
+deleteBtn.addEventListener('click',deleteFortune)
+allBtn.addEventListener('click',getAll)
--- assessment-backend/server/index.js
+++ assessment-backend/server/index.js
@@ -1,14 +1,22 @@
+//Require packages
 const express = require("express");
 const cors = require("cors");
 
+//Create app instance
 const app = express();
 
+//Set up middleware
 app.use(cors());
-
 app.use(express.json());
 
-const { getFortune } = require('./controller')
+//Endpoints
+const { getFortune,postFortune,deleteFortune,getAll } = require('./controller')
 
 app.get("/api/fortune", getFortune);
+app.post('/api/fortune',postFortune);
+app.delete('/api/fortune/:id',deleteFortune);
+app.get('/api/fortune',getAll)
 
+
+//Open port for my server
 app.listen(4000, () => console.log("Server running on 4000"));
